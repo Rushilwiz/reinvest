e4e1a605c84a50b5e7b94c0db43699034965600e
Rushil Umaretiya <rushilwiz@gmail.com>
2021-01-30 23:24:24 -0500

    feat(backend): finished backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,56 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from . import models
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        print("I WAS CALLED!")
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        profile = models.Profile.objects.create(user=user)
+        profile.save()
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Stock
+        fields = ('ticker', 'buy_price', 'quantity', 'uuid')
+
+class StockCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Stock
+        fields = ('user', 'ticker', 'buy_price', 'quantity', 'uuid')
+
+class CharitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Charity
+        fields = ('ein', 'name', 'sub_name', 'city', 'state')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    charity = CharitySerializer()
+    stocks = StockSerializer(many=True)
+    
+    class Meta:
+        model = models.Profile
+        fields = ('user', 'charity', 'nickname', 'profile_pic', 'percentage', 'stocks')
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,16 @@
 from rest_framework_jwt.views import obtain_jwt_token
 
-from django.urls import path
 from . import views
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 urlpatterns = [
-    path('token/', obtain_jwt_token)
+    path('charity/<int:pk>', views.CharityViewSet.as_view({'get': 'retrieve'})),
+    path('charity', views.CharityViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('stock/<uuid:pk>', views.StockViewSet.as_view({'get': 'retrieve'})),
+    path('stock', views.StockViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('profile/create', views.UserProfileCreate.as_view()),
+    path('profile', views.UserProfileDetail.as_view()),
+    path('token', obtain_jwt_token)
 ]
