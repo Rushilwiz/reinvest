38577a83f7da3a170807480b601c9078ef53f5c7
Rushil Umaretiya <rushilwiz@gmail.com>
2021-01-31 10:31:56 -0500

    fix(backend): fixed urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,11 +10,11 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('charity/<int:pk>', views.CharityViewSet.as_view({'get': 'retrieve'})),
-    path('charity/', views.CharityViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('charity', views.CharityViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('stock/<uuid:pk>', views.StockViewSet.as_view({'get': 'retrieve'})),
-    path('stock/', views.StockViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('stock', views.StockViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('profile/create', views.UserProfileCreate.as_view()),
-    path('profile/', views.UserProfileDetail.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile', views.UserProfileDetail.as_view()),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
