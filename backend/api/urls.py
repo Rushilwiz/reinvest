dc37793c7002d7e9b62cb47a3902501e0eab34b1
Rushil Umaretiya <rushilwiz@gmail.com>
2021-01-31 10:15:01 -0500

    feat(backend): changed to simple jwt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,20 @@
-from rest_framework_jwt.views import obtain_jwt_token
-
 from . import views
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('charity/<int:pk>', views.CharityViewSet.as_view({'get': 'retrieve'})),
-    path('charity', views.CharityViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('charity/', views.CharityViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('stock/<uuid:pk>', views.StockViewSet.as_view({'get': 'retrieve'})),
-    path('stock', views.StockViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('stock/', views.StockViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('profile/create', views.UserProfileCreate.as_view()),
-    path('profile', views.UserProfileDetail.as_view()),
-    path('token', obtain_jwt_token)
+    path('profile/', views.UserProfileDetail.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
